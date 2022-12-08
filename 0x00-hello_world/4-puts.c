d211424e9ef495cd368ac6e24806a0933ad37924
OChibu <beze4luv@yahoo.com>
2022-12-08 11:26:11 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main -Entry
  *
  * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	Return(0);
+puts("\"Programming is like building a multilingual puzzle\n");
+Return(0);
 }
