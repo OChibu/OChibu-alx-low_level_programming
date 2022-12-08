acd88d92aa9a1fc337fc52c9f32caf592b1e1aff
OChibu <beze4luv@yahoo.com>
2022-12-08 12:32:59 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main -Entry
  *
- * Return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
-Return(0);
+return(0);
 }
