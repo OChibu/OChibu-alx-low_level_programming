a7d019699cf92e9f606765dbeb358f6f8152836f
OChibu <beze4luv@yahoo.com>
2022-12-11 09:21:41 -0800

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,17 @@ for (m = 49; m <= 57; m++)
 if (m > n)
 {
 putchar(n);
-}
 putchar(m);
-}
 if (n != 56 || m != 57)
 {
 putchar(',');
-}
 putchar(' ');
+}
+
+}
+
+}
+
 }
 putchar('\n');
 return (0);
