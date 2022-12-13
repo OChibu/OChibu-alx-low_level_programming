6676598b5c56a04fab788b7822f9eb458f8d52fd
OChibu <beze4luv@yahoo.com>
2022-12-13 11:02:41 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
 #include "main.h"
 /**
- * main - prints _putchar, followed by a new line
+ * main - prints _putchar
+ * Is written by Ochibu
  * Return: 0
 */
-int _putchar(char);
+
+int main(void)
 {
-char *sh = "_putchar";
-while (*sh)
-{
-putchar(*sh);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
