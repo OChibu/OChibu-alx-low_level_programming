679f3489f1f910666ef98b71811c2000e7e533ea
OChibu <beze4luv@yahoo.com>
2022-12-14 02:56:46 -0800

    void print_times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,13 +5,13 @@
  */
 void print_times_table(int n)
 {
-int i, j, k;
+int a, b, op;
 if (n >= 0 && n <= 15)
 {
-for (i = 0; i <= n; i++)
+for (a = 0; a <= n; a++)
 {
-_putchar(48);
-for (j = i; j <= i; j++)
+_pchar(48);
+for (b = 1; b <= n; b++)
 {
 op = a * b;
 _putchar(44);
