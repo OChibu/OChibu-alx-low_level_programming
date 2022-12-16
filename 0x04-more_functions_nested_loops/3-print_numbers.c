97596777eed298b4de03fd70611663941dcb748c
OChibu <beze4luv@yahoo.com>
2022-12-16 02:13:46 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - print 0123456789
- * owned by Ochibu
+ * print_numbers - a function that prints the numbers, from 0 to 9
  * Return: void
  */
 
 void print_numbers(void)
 {
+
 char c;
+
 for (c = '0'; c <= '9'; c++)
+{
 _putchar(c);
+}
 _putchar('\n');
 }
