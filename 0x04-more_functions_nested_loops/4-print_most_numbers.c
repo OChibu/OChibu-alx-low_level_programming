fe707fa7581dc6fabcd7e370dfac4a386a7c3ed8
OChibu <beze4luv@yahoo.com>
2022-12-16 03:57:33 -0800

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
+ * print_most_numbers - prints numbers between 0 to 9
  * owned by Ochibu
- * Return: 0-9, excluding 2 and 4, followed by new line
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
-char c;
+int i;
 
-for (c = '0'; c <= '9'; c++)
+for (i = 0; i <= 9; i++)
 {
-if (!(c == '2' || c == '4'))
-_putchar(c)
+if ((i == 2) || (i == 4))
+	continue;
+else
+_putchar(i + '0');
 }
 _putchar('\n');
 }
