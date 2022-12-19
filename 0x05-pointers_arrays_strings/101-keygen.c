1cf18c883c78f75a25d902c6438b4afc3aa9526d
OChibu <beze4luv@yahoo.com>
2022-12-19 11:59:25 -0800

    random valid passwords for the program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -37,6 +37,14 @@ int main(void)
 				break;
 			}
 		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
 	}
 	password[index] -= diff_half1;
 	return (0);
